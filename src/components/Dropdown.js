4b8e01a821b554cbafa3fec80975f3d83c4bfd8b
Francisco <danielarcesoftware@gmail.com>
2021-05-27 00:09:56 -0700

    Progreso en el hero page y inicio de diseño en el nav principal

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Dropdown = (isOpen, toggle) => {
+const Dropdown = ({isOpen, toggle}) => {
     return (
-        <ul className={isOpen ? 'grid grid-rows-5 text-center items-center bg-red-800 text-white' : 'hidden'} onClick={toggle}>
+        <ul className={isOpen ? 'grid grid-rows-6 text-center items-center bg-green-800 text-white' : 'hidden'} 
+        onClick={toggle}>
                 <li className="p-4" to='/' value='About'>About</li>
                 <li className="p-4 " to='/menu' value='Home'>Study History</li>
                 <li className="p-4" to='/about' value='Home'>Work History</li>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Logo from '../images/Logo_Light.png'
+import Logo1 from '../images/Logo_Dark.png'
+
 const Navbar = ({toggle}) => {
     return (
-        <nav className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono' role='navigation'>
-            <input type='button'  className='pl-8'value='Home'/>
+        <nav className='flex justify-between items-center h-16 bg-black text-white relative shadow-sm font-mono' role='navigation'>
+            <div><img src={Logo1} alt="coding" className="personal-logo" /></div>
             <ul className="px-4 cursor-pointer md:hidden" onClick={toggle}>
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
             </ul>
-            <ul className="pr-8 md:block hidden">
-            <li className="p-4" to='/' value='About'>About</li>
+            <ul className="pr-8 md:flex hidden ul-nav-hero">
+                <li className="p-4" to='/' value='About'>About</li>
                 <li className="p-4 " to='/menu' value='Home'>Study History</li>
                 <li className="p-4" to='/about' value='Home'>Work History</li>
                 <li className="p-4" to='/about' value='Home'>Hobbies</li>
