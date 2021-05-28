9aa8047f98d1a03e2394bffc1e5c0477afaeeb61
Francisco <danielarcesoftware@gmail.com>
2021-05-27 22:41:31 -0700

    Hero Page Improvement

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 import Logo from '../images/Logo_Light.png'
 import Logo1 from '../images/Logo_Dark.png'
 
+// let audio = new Audio('../audio/beep.mp3');
+
 const Navbar = ({toggle}) => {
     return (
         <nav className='flex justify-between items-center h-16 bg-black text-white relative shadow-sm font-mono' role='navigation'>
-            <div><img src={Logo1} alt="coding" className="personal-logo" /></div>
+            <div><img src={Logo} alt="coding" className="personal-logo" /></div>
             <ul className="px-4 cursor-pointer md:hidden" onClick={toggle}>
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
